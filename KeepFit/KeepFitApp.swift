7178f09897e802d4b5d5a58ffa76d8a94627d07d
Steven Williams <stevencw@usc.edu>
2021-03-20 16:08:08 -0700

    Initial Commit

---
+++ KeepFit/KeepFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepFitApp.swift
+//  KeepFit
+//
+//  Created by Steven Williams on 3/20/21.
+//
+
+import SwiftUI
+
+@main
+struct KeepFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
